35facd5640fe2cc558fe23ce3c2b588a2f5d6f64
heller <heller@Si-L-PC>
2015-02-07 21:26:38 +0800

    Java Serialize Basic

---
+++ src/main/java/ch15/se08/Person.java
@@ -0,0 +1,35 @@
+package ch15.se08;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	private static final long serialVersionUID = 8097433699060774523L;
+	
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		System.out.println("使用了Person类的有参构造器来创建了一个实例");
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
---
+++ src/main/java/ch15/se08/WriteReadObjectTest.java
@@ -0,0 +1,35 @@
+package ch15.se08;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+
+public class WriteReadObjectTest {
+	
+	@Test
+	public void testWriteObject() throws FileNotFoundException, IOException {
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object.txt"))) {
+			Person p = new Person("heller", 18);
+			
+			// 将该Person对象写入文件（序列化）
+			oos.writeObject(p);
+			System.out.println("将Person对象序列化存储到文件中");
+		}
+	}
+	
+	@Test
+	public void testReadObject() throws FileNotFoundException, IOException, ClassNotFoundException {
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("object.txt"))) {
+			//  下面这行代码并没有打印（使用了Person类的有参构造器来创建了一个实例），表明通过反序列化还原出原来的对象时并不会取调用构造方法
+			Person p = (Person)ois.readObject();
+			System.out.println(p.getName()+" " + p.getAge());
+		}
+	}
+	
+}
