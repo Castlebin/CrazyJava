133e06dfe7191da4ed0c27b73643791fc5ea24f7
a <a@tp.com>
2015-04-14 17:42:20 +0800

    add idea .gitignore

--- src/main/java/ch07/se05/StringReg.java
+++ src/main/java/ch07/se05/StringReg.java
@@ -9,7 +9,6 @@ public class StringReg {
 				"regular expressions now expressing in Java",
 				"Java represses oracular expressions" 
 				};
-		
 		for(String msg : msgs) {
 			System.out.println(msg.replaceFirst("re\\w*", "哈哈:)"));
 			System.out.println(Arrays.toString(msg.split(" ")));
